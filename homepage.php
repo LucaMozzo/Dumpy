0619ba130c18b84b0ffdf2190423bf68e313f66f
Luca <lucamozzo21173@gmail.com>
2016-01-15 22:40:13 +0000

    added Javascript function to generate class code

--- homepage.php
+++ homepage.php
@@ -8,11 +8,6 @@
 
 <style>
 	
-	.pietro{
-		
-		background-color: #ffff;
-
-	}
 	.vertical-center{
 		
 		margin-top: 9%;
@@ -32,20 +27,19 @@
 </style>
     <!-- Bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-
   </head>
-   <nav class="navbar navbar-default">
+  <nav class="navbar navbar-default">
   <div class="container-fluid">
-    <div class="navbar-header">
-                <img src="dumpy-logo.png"class="pull-left" height="35px" width="35px"> </a>
-
-      <a class="navbar-brand" href="#">  Dumpy </a>
-</nav>
+	  <div class="navbar-header">
+		  <img src="dumpy-logo.png"class="pull-left" height="35px" width="35px"> </a>
+		  <a class="navbar-brand" href="#">  Dumpy </a>
+	  </div>
+  </nav>
 
   <body class="pietro">
 	  <div class="container">
 	 <div class="text-center">
-<div class="jumbotron vertical-center">
+	<div class="jumbotron vertical-center">
   <h1>Hey, learner!</h1>
   <p>Benefit of lots of resources, join an existing group or create your own.</p>
   <input class="btn btn-primary" type="button" onclick="newClass()" value="Create a new Classroom"></input>
