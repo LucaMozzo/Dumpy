bb781ceaa098f48a6df8c7d1d182e4df78043baa
Luca <lucamozzo21173@gmail.com>
2016-01-15 23:00:10 +0000

    added title blur image in background

--- homepage.php
+++ homepage.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, max-scale=1, user-scalable=no"/>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
+	<title>Dumpy</title>
 	<style>
 		.vertical-center{
 			margin-top: 9%;
@@ -20,6 +20,27 @@
 			margin-bottom: 7px;
 		}
 
+		.background-image {
+			background-image: url('http://666a658c624a3c03a6b2-25cda059d975d2f318c03e90bcf17c40.r92.cf1.rackcdn.com/unsplash_527bf56961712_1.JPG');
+			background-size: cover;
+			display: block;
+			filter: blur(5px);
+			-webkit-filter: blur(5px);
+			height: 800px;
+			left: 0;
+			position: fixed;
+			right: 0;
+			z-index: 1;
+		}
+
+		.container {
+			top: 10px;
+			left: 0;
+			position: fixed;
+			right: 0;
+			z-index: 2;
+			padding: 0 10px;
+		}
 	</style>
 </head>
 <nav class="navbar navbar-default">
@@ -32,6 +53,7 @@
 </nav>
 
 <body class="pietro">
+<div class="background-image"></div>
 <div class="container">
 	<div class="text-center">
 		<div class="jumbotron vertical-center">
