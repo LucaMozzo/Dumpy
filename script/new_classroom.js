f97c5e6f7efe3fe7a9b4c5d8df36d509984c247a
Luca <lucamozzo21173@gmail.com>
2016-01-16 15:00:11 +0000

    script added

---
+++ script/new_classroom.js
@@ -0,0 +1,18 @@
+//creates the class code from the class name
+function generateClassCode(){
+    var cname = document.getElementById("classname").value;
+    var str = "";
+    if(cname.search(" ") != -1) {
+        str += cname.slice(0, 3);
+        str += cname.slice(cname.search(" ") + 1, cname.search(" ") + 4);
+    }
+    else {
+        str+= cname;
+    }
+    str += Math.floor(Math.random() * 10);
+    str += Math.floor(Math.random() * 10);
+    while(str.search(" ") != -1) {
+        str = str.replace(" ", ""); //to make sure the classcode has no spaces in it
+    }
+    document.getElementById("classcode").value = str;
+}
