2ce085417e250fcc66ba7c4f60f50c0fc9d618d2
Luca <lucamozzo21173@gmail.com>
2016-01-16 15:13:56 +0000

    classroom minor changes

--- new_classroom.php
+++ new_classroom.php
@@ -13,42 +13,30 @@
 			margin-top: 9%;
 		}
 	</style>
-	<script>
-		function generateClassCode(){
-			var cname = document.getElementById("classname").value;
-			var str = "";
-			if(cname.search(" ") != -1) {
-				str += cname.slice(0, 3);
-				str += cname.slice(cname.search(" ") + 1, cname.search(" ") + 4);
-			}
-			else {
-				str+= cname;
-			}
-			str += Math.floor(Math.random() * 10);
-			str += Math.floor(Math.random() * 10);
-			while(str.search(" ") != -1) {
-				str = str.replace(" ", ""); //to make sure the classcode has no spaces in it
-			}
-			document.getElementById("classcode").value = str;
-		}
-	</script>
+	<script src="script/new_classroom.js"> </script>
 </head>
 <body>
 <div class="container vertical-center text-center">
 	<h1>Let's create a new Classroom!</h1>
 	<form method="post" action="index.php">
-		<div class="form-group">
-			<input type="text" id="classname"  class="form-control" name ="class_name" placeholder="Give a name to the classroom" onchange="generateClassCode()"/>
-		</div>
-		<div class="input-group">
-			<span class="input-group-addon">https://dumpy.altervista.org/classroom/</span>
-			<input id="classcode" type="text" name="class_code" class="form-control" placeholder="Create a unique code for your class" disabled/>
-			<span class="input-group-btn">
-				<input type="submit" class="btn btn-default" value="Edit" id ="edit-id-btn"/>
-			</span>
-		</div>
-		<div class="text-center">
-			<input type="submit" class="btn btn-success" name="submit" value="Create classroom"/>
+		<div class="col-sm-8 center col-md-8 col-md-offset-2">
+			<div class="form-group">
+				<input type="text" id="classname"  class="form-control" name ="class_name" placeholder="Give a name to the classroom" onchange="generateClassCode()"/>
+			</div>
+			<div class="form-group">
+				<div class="input-group">
+					<span class="input-group-addon">https://dumpy.altervista.org/classroom/</span>
+					<input id="classcode" type="text" name="class_code" class="form-control" placeholder="Create a unique code for your class" disabled/>
+					<span class="input-group-btn">
+						<input type="submit" class="btn btn-default" value="Edit" id ="edit-id-btn"/>
+					</span>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="text-center">
+					<input type="submit" class="btn btn-success" name="submit" value="Create classroom"/>
+				</div>
+			</div>
 		</div>
 	</form>
 </div>
